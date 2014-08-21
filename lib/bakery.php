c07c62078e3f379fdd9cedaaba16c9284d819ce8
Splug <m@splug.io>
2014-08-21 18:19:38 -0400

    adding in basic bakery orm

---
+++ lib/bakery.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace bakery;
+
+/*
+
+ */
+class orm extends \PDO{
+
+    /*
+    
+     */
+    public function __construct($host, $db, $user, $pass, $type='mysql'){
+        parent::__construct("{$type}:host={$host};dbname={$db}", $user, $pass);
+        
+        $this->config['db'] = $db;
+        $this->config['type'] = $type;
+
+        // Set Attributes
+        $this->setAttrbitues();
+
+        // Load Tables
+        $this->getTables();
+
+    }
+
+    /*
+    
+     */
+    private function getTables(){
+        $tables = [];
+        $raw_tables = $this->query("show tables")->fetchAll(\PDO::FETCH_ASSOC);
+        foreach($raw_tables as $table){
+            $tables[] = $table['Tables_in_'.$this->config['db']];
+        }
+
+        $this->tables = $tables;
+    }
+
+    /*
+    
+     */
+    public function setAttrbitues(){
+        //$this->setAttribute(\PDO::ATTR_AUTOCOMMIT,0);
+        $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->setAttribute(\PDO::ATTR_CASE, \PDO::CASE_NATURAL);
+    }
+
+    private function getTableSchema($table){
+        $stmt = $this->prepare("SHOW COLUMNS FROM `$table`");
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    private function createOrm($table, $schema){
+        
+        $orm = new table($table, $this);
+
+        if(!empty($schema) && is_array($schema)){
+            foreach($schema as $column){
+                $orm->setColumn($column['Field'], 'value', $column['Default']);
+                $orm->setColumn($column['Field'], 'type', $column['Type']);
+                $orm->setColumn($column['Field'], 'null', ($column['Null'] == 'YES' ? true : false));
+                $orm->setColumn($column['Field'], 'key', ($column['Key'] == 'PRI' ? true : false));
+            }
+        }
+
+        return $orm;
+    }
+    /*
+    
+     */
+    public function findOrCreate($table, $column, $value){
+        
+        if( in_array($table, $this->tables) === false ){
+            return false;
+        }
+
+        $schema = $this->getTableSchema($table);
+        $orm = $this->createOrm($table, $schema);
+
+        $stmt = $this->prepare("SELECT * FROM {$table} WHERE {$column} = :value");
+        $stmt->setFetchMode(\PDO::FETCH_INTO, $orm);
+        $stmt->execute(array(':value' => $value));
+        $results = $stmt->fetch();
+
+        if(empty($results)){
+            return $orm;
+        }
+
+        return $results;
+
+    }
+
+}
+
+class table {
+    
+    private $columns = [],
+            $primary_key,
+            $table,
+            $pdo;
+
+    public function __construct($table, $conn){
+        
+        $this->table = $table;
+        $this->pdo = $conn;
+
+        return $this;
+    }
+
+    public function setColumn($column, $attr, $value){
+        if(!isset($this->columns[$column])){
+            $this->columns[$column] = [];
+        }
+
+        if( $attr == 'key' && $value === true ){
+            $this->primary_key = $column;
+        }
+
+        $this->columns[$column][$attr] = $value;
+    }
+
+    public function __set($column, $args){
+        $this->columns[$column]['value'] = $args;
+    }
+
+    public function __get($column) {
+        return $this->columns[$column]['value'];
+    }
+
+    public function save(){
+        $prepared_columns = [];
+        $prepared_values = [];
+
+        $primary = $this->columns[$this->primary_key];
+
+        if(!empty($primary['value'])){
+            $type = "UPDATE {$this->table} SET ";
+            $where = " WHERE {$this->primary_key} = :{$this->primary_key}";
+            $prepared_values[":{$this->primary_key}"] = $primary['value'];
+        }
+        else{
+            $type = "INSERT INTO {$this->table} SET ";   
+        }
+
+        foreach($this->columns as $column => $meta){
+            if($meta['key'] != 1){
+               $prepared_columns[] = "{$column}=:$column";
+               $prepared_values[":{$column}"] = $meta['value'];
+            }
+        }       
+
+        $stmt = $this->pdo->prepare("{$type}".implode(", ", $prepared_columns)."{$where}");
+        $stmt->execute($prepared_values);
+        
+        return $stmt->rowCount();
+    }
+
+}

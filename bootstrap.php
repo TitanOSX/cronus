7fe46639bf4cc8bace61d15f9191e14f3fff1c5d
Splug <m@splug.io>
2014-08-24 00:53:09 -0400

    added observer hook and checks for module/watcher

--- bootstrap.php
+++ bootstrap.php
@@ -15,7 +15,6 @@ $app->config(array(
     'templates.path' => 'views/'
 ));
 
-
 $app->post('/connect/:serial', function($serial) use ($app){
     
     if(!strstr($_SERVER['HTTP_USER_AGENT'], "ctznOSX")){
@@ -59,13 +58,14 @@ $app->post('/connect/:serial', function($serial) use ($app){
         $response->body(json_encode(["response" => "successful", "error" => null]));
     }
     else{
-        $response->body(json_encode(["response" => "successful", "error" => "_errorSavingRecord"]));        
+        $response->body(json_encode(["response" => "failure", "error" => "_errorSavingRecord"]));        
     }
     
     //print $serial;
 })->name('connect');
 
 $app->post('/register/:serial', function($serial) use ($app){
+
     if(!strstr($_SERVER['HTTP_USER_AGENT'], "ctznOSX")){
         die("_invalidRequest");
     }
@@ -92,7 +92,6 @@ $app->post('/register/:serial', function($serial) use ($app){
     $device->os_version = $app->request->post('os_version');
     $device->os_build = $app->request->post('os_build');
     
-    
     if($device->save()){
         $response = $app->response();
         
@@ -103,10 +102,85 @@ $app->post('/register/:serial', function($serial) use ($app){
         $response->body(json_encode(["response" => "successful", "error" => null]));
     }
     else{
-        $response->body(json_encode(["response" => "successful", "error" => "_errorSavingRecord"]));        
+        $response->body(json_encode(["response" => "failure", "error" => "_errorSavingRecord"]));        
     }
 
 })->name("register");
+
+
+$app->post('/observer/:serial', function($serial) use ($app){
+    
+    # Set Response Object
+    $response = $app->response();
+
+    if(!strstr($_SERVER['HTTP_USER_AGENT'], "ctznOSX")){
+        die("_invalidRequest");
+    }
+
+    if($_POST['ping'] == 'ping'){
+        $response->status(203);
+        $response->body(json_encode(["response" => "successful", "error" => null]));        
+        return;
+    }
+
+    if( hash("sha256", $_POST['stream']) != $_POST['digest']){
+        $response->status(417);        
+
+        $response->body(json_encode(["response" => "failure", "error" => "_couldNotValidateStream"]));        
+        return;
+    }
+    
+    $app->response['Content-Type'] = 'application/json';   
+
+    $stream = zlib_decode($_POST['stream']);
+
+    # Create new device if it doesnt exist
+    $device = $app->db->find_devices_by_serial($serial);
+
+    $module_data = json_decode($stream);
+    $table = $module_data->module;
+
+    try{
+        $module_template = $app->db->create("module_".$table);
+    
+
+        $successes = 0;
+        $required = sizeof($module_data->data);
+
+        foreach($module_data->data as $row){
+            $module = $module_template;
+
+            $module->device_id = $device->id;
+
+            foreach($row as $column => $value){
+                if($module->hasColumn($column)){
+                    $module->{$column} = $value;
+                }
+            }
+            if($module->save()){
+                $successes++;
+            }
+        }
+    }
+    catch(\Exception $e){
+        $response->status(403);        
+        $response->body(json_encode(["response" => "failure", "error" => "_moduleSchemaDoesNotExist"]));                
+        return;
+    }
+
+    if($required == $successes){       
+        $response['X-Powered-By'] = 'ctznOSX_CDM';
+        $response->status(202);
+        // etc.
+
+        $response->body(json_encode(["response" => "successful", "error" => null]));
+    }
+    else{
+        $response->status(504);        
+        $response->body(json_encode(["response" => "failure", "error" => "_errorUploadingModuleData"]));        
+    }
+
+})->name("observer");
 
 /*
 $app->notFound(function () use ($app) {

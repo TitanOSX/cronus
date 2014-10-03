548339eab739aa6e8b1a2594e98ed251713f2ca6
Mike Mackintosh <m@splug.io>
2014-10-03 15:31:17 -0400

    fixed web/index

--- web/index.php
+++ web/index.php
@@ -1,3 +1,8 @@
 <?php
+// Local Router for PHP Dev Stuff
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
 
 require_once(dirname(dirname(__FILE__)).'/bootstrap.php');
